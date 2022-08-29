2cfb7775aa9c80ab2ca2529a8d25102a4138a368
przemyslaw.zygmanowski <przemyslaw.zygmanowski@makolab.com>
2022-08-29 23:05:14 +0200

    first commit folder structure depedencies install

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { UserForm } from './pages/UserForm';
+
+function App() {
+  return (
+    <>
+      <UserForm></UserForm>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/UserForm.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AddUserForm } from '../components/AddUserForm';
+
+export const UserForm = () => {
+  return <AddUserForm></AddUserForm>;
+};
